84328616ebb57b026da09fff5ac1a39b7abacccd
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2023-08-05 02:44:03 +0530

    feat: add api call to ml model endpoint
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

--- api/get_predicted_pm25.go
+++ api/get_predicted_pm25.go
@@ -1,7 +1,53 @@
 package api
 
-import "github.com/sadityakumar9211/clean-route-backend/models"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/sadityakumar9211/clean-route-backend/models"
+	"github.com/spf13/viper"
+)
+
+type Post struct {
+	FPM float64 `json:"fpm"`
+}
 
 func GetPredictedPm25(inputFeatures models.FeatureVector, delayCode uint8) (float64, error) {
-	return 1, nil
+
+	awsModelEndpoint, awsModelEndpointError := viper.Get("AWS_MODEL_ENDPOINT").(string)
+	if !awsModelEndpointError {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	fmt.Println("The Query url is: ", awsModelEndpoint)
+
+	inputFeatures.DelayCode = delayCode
+	jsonData, err := json.Marshal(inputFeatures)
+	checkErrNil(err)
+
+	r, err := http.NewRequest("POST", awsModelEndpoint, bytes.NewBuffer(jsonData))
+	checkErrNil(err)
+
+	r.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	checkErrNil(err)
+
+	defer resp.Body.Close()
+
+	post := &Post{}
+
+	err = json.NewDecoder(resp.Body).Decode(post)
+	checkErrNil(err)
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("Error while making calling API endpoint", err)
+		return 0, err
+	}
+
+	return post.FPM, nil
 }
--- models/feature_vector.go
+++ models/feature_vector.go
@@ -2,13 +2,14 @@ package models
 
 // This is the struct which contains all the relevant features (ITEMP, IRH, IWD, IWS, IPM, FTEMP, FRH, FWD, FWS)
 type FeatureVector struct {
-	ITEMP float64 `json:"ITEMP"`
-	IRH   float64 `json:"IRH"`
-	IWD   float64 `json:"IWD"`
-	IWS   float64 `json:"IWS"`
-	IPM   float64 `json:"IPM"`
-	FTEMP float64 `json:"FTEMP"`
-	FRH   float64 `json:"FRH"`
-	FWD   float64 `json:"FWD"`
-	FWS   float64 `json:"FWS"`
+	ITEMP     float64 `json:"ITEMP"`
+	IRH       float64 `json:"IRH"`
+	IWD       float64 `json:"IWD"`
+	IWS       float64 `json:"IWS"`
+	IPM       float64 `json:"IPM"`
+	FTEMP     float64 `json:"FTEMP"`
+	FRH       float64 `json:"FRH"`
+	FWD       float64 `json:"FWD"`
+	FWS       float64 `json:"FWS"`
+	DelayCode uint8   `json:"delayCode"`
 }
